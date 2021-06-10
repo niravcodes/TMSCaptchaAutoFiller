5bdf973b924b8deae9aa7713bbc77de4150028dc
thekshitiz <dikhwachha@gmail.com>
2021-06-10 16:10:02 +0545

    added autoLogin - 2 sec

--- contentScript.js
+++ contentScript.js
@@ -1,8 +1,24 @@
 {
-    const $ = _ => document.getElementById(_) //aliasing a long fn call
+    const $ = (_) => document.getElementById(_); //aliasing a long fn call
     function l() {
-        $("captchaEnter").value = $("randomfield").value
-        $("captchaEnter").dispatchEvent(new Event("input"))
+        $('captchaEnter').value = $('randomfield').value;
+        $('captchaEnter').dispatchEvent(new Event('input'));
     }
     window.onload = l;
+    timer();
+}
+
+// auto login
+
+function autoClick() {
+    var button = document.getElementsByClassName(
+        'btn btn-primary float-sm-right'
+    );
+    var button = button[0];
+    button.click();
+}
+
+function timer() {
+    var clock;
+    clock = setTimeout(autoClick, 2000);
 }
